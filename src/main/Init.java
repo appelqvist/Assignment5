be62de9bc4ec8c46c8c3b974f3d9cdde8ad89e01
Andreas Appelqvist <andreas.appelqvist@gmail.com>
2016-01-11 21:55:46 +0100

    Initial commit

---
+++ src/main/Init.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * Created by Andreas Appelqvist on 2016-01-11.
+ */
+public class Init {
+    public static void main(String[] args) {
+        System.out.println("Init");
+    }
+}
